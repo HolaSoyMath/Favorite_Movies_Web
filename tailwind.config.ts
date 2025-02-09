f47ef4eb0b894caac785426f3ad6dd2886a93464
Matheus <theo.guimaraes@hotmail.com>
2025-02-09 13:17:15 -0300

    add PostCSS configuration, global styles, and utility functions; update TypeScript and Tailwind CSS settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  darkMode: ["class"]
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"], // necessário para funcionar com next-themes
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./presentation/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@shadcn/ui/components/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@shadcn/ui/dist/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#6366f1",
+          darkBg: "#0f172a",
+          darkCard: "#1e293b"
+        }
+      }
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: []
+};
